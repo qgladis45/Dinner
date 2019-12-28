71d66aeb1ac5b8bcc0c2decdc7eee82e3408ab5e
Annie Hsieh <anniehsieh419@gmail.com>
2019-12-28 20:50:23 +0800

    Update final.py

--- final.py
+++ final.py
@@ -8,21 +8,6 @@ import webbrowser
 from PIL import Image, ImageTk
 from urllib.request import urlopen
 import io
-
-#網路連線檢查
-def popup_showinfo():
-    showinfo("溫馨小提示", "NO INTERNET CONNECTION") 
-
-def check_internet():
-    url='http://www.google.com/'
-    timeout=5
-    try:
-        _ = requests.get(url, timeout=timeout) #以谷歌測試
-        return True
-    except requests.ConnectionError:
-        popup_showinfo() #如果沒網路，以視窗顯示
-    return False
-check_internet()
 	
 '''爬蟲'''
 url = "https://kma.kkbox.com/charts/daily/song?cate="
@@ -44,7 +29,6 @@ attr_spotify = {'class': 'chart-table-track'}
 rank_spotify = soup_spotify.find_all('td', attrs = attr_spotify)     # 找到html裡面的td標籤
 
 spotify_num = 0
-
 
 # spotify的排名
 for i in rank_spotify:
@@ -78,13 +62,11 @@ for o in (man_rank, eng_rank, jap_rank, kor_rank):
     cover_url = cover_url.replace('\\' , '')
     topone_cover.append(cover_url)
 
-
     # 找到前五歌名和歌手
     rank = soup.find_all('meta', attrs=attr)       # 找到html裡面的meta標籤
     rank_str = rank[0]['content']                  # 找到排行榜的部分
     rank_str = rank_str[(rank_str.find('：')+1):]  # 只抓取歌單的部分
     rank_list = rank_str.split('、')               # 把str轉成list
-
 
     # list中0,2,4,6,8為歌名; 1,3,5,7,9為歌手
     for i in rank_list:
@@ -104,7 +86,6 @@ for o in (man_rank, eng_rank, jap_rank, kor_rank):
     # 把前後有空格的整理乾淨
     for i in range(10):
         o[i] = o[i].strip()
-
 
 spotify_pic_url = "http://www.scdn.co/i/_global/twitter_card-default.jpg"
 
@@ -182,7 +163,7 @@ class Ranking(tk.Frame):
 ranking = Ranking()
 ranking.master.title("KKbox Ranking")
 ranking.master.geometry('-30-50')      # 視窗設在右下角
-ranking.master.attributes('-alpha', 1)  # 不透明
+ranking.master.attributes('-alpha', 1) # 不透明
 ranking.master.resizable(0, 0)         # 鎖定視窗大小
 ranking.configure(bg='Black')          # 背景顏色
 ranking.master.overrideredirect(True)  # 刪除標題欄
