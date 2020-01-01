1328b6e96560a6ba083efbd03661f3ce11a864e3
kevinchang1213 <59016026+kevinchang1213@users.noreply.github.com>
2020-01-02 01:02:51 +0800

    Update final_chrome.py

--- final_chrome.py
+++ final_chrome.py
@@ -9,9 +9,7 @@ from PIL import Image, ImageTk
 from urllib.request import urlopen
 import io
 from selenium import webdriver
-from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
